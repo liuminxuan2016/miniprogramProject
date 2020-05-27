02b80b2277d611eb93d9d4fc0a979bd64ad26c0c
LiuMinXuan <liuminxuan2016@gmail.com>
2020-05-27 11:21:47 +0800

    0527

---
+++ pages/index/article/article.js
@@ -0,0 +1,109 @@
+// pages/index/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        tabs:[
+            {
+              id:0,
+              name:"首页",
+              isActive:true,
+              tabpages:"/pages/index/index"
+            },
+            {
+              id:1,
+              name:"文章",
+              isActive:false,
+              tabpages:"/pages/index/article/article"
+            },
+            {
+              id:2,
+              name:"视频",
+              isActive:false,
+              tabpages:"/pages/index/video/video"
+            },
+            {
+              id:3,
+              name:"图片",
+              isActive:false,
+              tabpages:"/pages/index/picture/picture"
+            },
+            {
+              id:4,
+              name:"关于",
+              isActive:false,
+              tabpages:"/pages/index/about/about"
+            }
+          ] 
+    },
+    hanldeItemTap:function(e){
+        const {index}=e.currentTarget.dataset;
+      //let tabs=JSON.parse(JSON.stringify(this.data.tabs));
+      let {tabs}=this.data;
+      tabs.forEach((v,i)=>i===index?(v.isActive=true && wx.navigateTo({
+        url:v.tabpages,
+      })):v.isActive=false );
+      this.setData(
+        {
+          tabs
+        }
+      )
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
